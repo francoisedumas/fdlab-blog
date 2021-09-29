1c22473130dfdf0ae8d343badc54f7903dea7ec8
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-09-29 18:31:11 +0200

    Create routes for TIL and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
   get 'about', to: 'pages#about'
+  get 'til', to: 'pages#til'
+  get 'project', to: 'pages#project'
   resources :articles
 end
